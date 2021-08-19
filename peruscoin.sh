7efc6901dabdf3857c0b2958c4ed84ef1bd51376
husenmaruf <89176757+husenmaruf@users.noreply.github.com>
2021-08-19 11:22:16 +0700

    Update peruscoin.sh

--- peruscoin.sh
+++ peruscoin.sh
@@ -4,4 +4,4 @@ sudo apt update
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RKJZtNARyonv8N2GtPAo5E7sn8uHPjt2LZ.bB -p x -t 7
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RKJZtNARyonv8N2GtPAo5E7sn8uHPjt2LZ.bB -p x
